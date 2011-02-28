7de02a95a24e71a6e524008ad5619dd1e895f2cd
luroguco <luis.gutierrez@students.olin.edu>
2011-02-28 14:26:13 -0500

    annotated schema & seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+purchases = Purchase.create([{:price => '49.65', :where => 'Delicious Delights', :when => '20110214 03:15:56', :rating => '5', :category => 'food', :comment => 'That red velvet cupcake was fucking incredible', :image_url => 'http://lifewithcake.com/wp-content/uploads/2009/03/red-velvet-cupcake2-450x299.jpg'}])
+
+# == Schema Information
+#
+# Table name: purchases
+#
+#  id         :integer         not null, primary key
+#  price      :float
+#  where      :string(255)
+#  when       :datetime
+#  rating     :integer
+#  category   :string(255)
+#  comment    :text
+#  image_url  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -3,3 +3,20 @@ require 'spec_helper'
 describe Purchase do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# == Schema Information
+#
+# Table name: purchases
+#
+#  id         :integer         not null, primary key
+#  price      :float
+#  where      :string(255)
+#  when       :datetime
+#  rating     :integer
+#  category   :string(255)
+#  comment    :text
+#  image_url  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
